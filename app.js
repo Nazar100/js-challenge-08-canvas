31f52afb542744cd61f1484e939a6917c3d7a632
Nazar100 <leshc005@umn.edu>
2020-10-08 12:51:09 +0300

    w

---
+++ app.js
@@ -0,0 +1,50 @@
+const canvasRef = document.querySelector("#draw");
+const ctx = canvasRef.getContext("2d");
+console.log(ctx, canvasRef);
+
+canvasRef.width = window.innerWidth;
+canvasRef.height = window.innerHeight;
+ctx.strokeStyle = "blue";
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
+ctx.lineWidth = 50;
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+
+function draw(e) {
+  if (!isDrawing) {
+    return;
+  }
+  ctx.strokeStyle = `hsl(${hue},100%,50%)`;
+
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  lastY = e.offsetY;
+  lastX = e.offsetX;
+  hue++;
+  if (hue >= 360) {
+    hue = 0;
+  }
+  if (ctx.lineWidth >= 100) {
+    console.log(typeof ctx.lineWidth);
+    ctx.lineWidth -= 1;
+  } else if (ctx.lineWidth <= 1) {
+    console.log(ctx.lineWidth);
+    ctx.lineWidth += 1;
+  }
+  ctx.lineWidth = hue;
+}
+canvasRef.addEventListener("mousemove", draw);
+canvasRef.addEventListener("mousedown", (e) => {
+  //   console.log(ctx.lineWidth, direction);
+  isDrawing = true;
+  lastY = e.offsetY;
+  lastX = e.offsetX;
+});
+canvasRef.addEventListener("mouseup", () => (isDrawing = false));
+canvasRef.addEventListener("mouseout", () => (isDrawing = false));
